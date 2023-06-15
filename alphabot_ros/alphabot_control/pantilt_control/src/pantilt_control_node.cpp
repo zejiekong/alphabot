ec54003633c340fa2ca63cc17193c585a4a3b4b2
alphakong <ze-jie-kong-@hotmail.com>
2023-06-15 06:07:25 +0000

    rm yolo

--- alphabot_ros/alphabot_control/pantilt_control/src/pantilt_control_node.cpp
+++ alphabot_ros/alphabot_control/pantilt_control/src/pantilt_control_node.cpp
@@ -1,8 +1,9 @@
 #include <sensor_msgs/JointState.h>
 #include <ros/ros.h>
 #include <ros/package.h>
-#include <detection_msgs/BoundingBoxes.h>
-#include <detection_msgs/BoundingBox.h>
+#include "vision_msgs/Detection2DArray.h"
+#include "vision_msgs/Detection2D.h"
+#include "vision_msgs/ObjectHypothesisWithPose.h"
 #include <vector>
 #include <signal.h>
 
@@ -21,21 +22,21 @@ void shutdownHandler(int sig)
     ros::shutdown();
 }
 
-void detect_callback(const detection_msgs::BoundingBoxesConstPtr msg)
+void detect_callback(const vision_msgs::Detection2DArrayConstPtr msg)
 {
-    int num_faces = msg->bounding_boxes.size();
+    int num_faces = msg->detections.size();
     int max_probability = 0;
     int max_num;
     for(int i = 0;i<num_faces;i++)
     {
-        if(msg->bounding_boxes[num_faces].probability > max_probability)
+        if(msg->detections[num_faces].results[0].score > max_probability)
         {
-            max_probability = msg->bounding_boxes[num_faces].probability;
+            max_probability = msg->detections[num_faces].results[0].score;
             max_num = i;
         }
     }
-    int centre_x = (msg->bounding_boxes[max_num].xmax + msg->bounding_boxes[max_num].xmin)/2;
-    int center_y = (msg->bounding_boxes[max_num].ymax + msg->bounding_boxes[max_num].ymin)/2;
+    int centre_x = msg->detections[max_num].bbox.center.x;
+    int center_y = msg->detections[max_num].bbox.center.y;
     int error_x = centre_x - 205; //-ve turn right, +ve turn left
     int error_y = center_y - 154; //-ve move up, +ve move down
     sensor_msgs::JointState cmd_msg;
@@ -84,6 +85,6 @@ int main(int argc, char* argv[])
         ROS_INFO("Unsuccesful loading of configuration file. Default threshold: %d",threshold);
     }
     pub = nh.advertise<sensor_msgs::JointState>("/alphabot/servo_cmd",1);
-    ros::Subscriber detect_sub = nh.subscribe("/alphabot/face_detection/coord",1,detect_callback);
+    ros::Subscriber detect_sub = nh.subscribe("/alphabot/face_detection/bouding_boxes",1,detect_callback);
     ros::spin();
 }
--- alphabot_ros/alphabot_perception/face_detection/src/face_detection_node.cpp
+++ alphabot_ros/alphabot_perception/face_detection/src/face_detection_node.cpp
@@ -1,6 +1,7 @@
 #include <cv_bridge/cv_bridge.h>
-#include <detection_msgs/BoundingBoxes.h>
-#include <detection_msgs/BoundingBox.h>
+#include "vision_msgs/Detection2DArray.h"
+#include "vision_msgs/Detection2D.h"
+#include "vision_msgs/ObjectHypothesisWithPose.h"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
@@ -11,7 +12,7 @@
 #include <std_msgs/Header.h>
 
 cv::CascadeClassifier faceCascade;
-ros::Publisher coord_pub, img_pub;
+ros::Publisher detect_pub, img_pub;
 
 void callback(const sensor_msgs::CompressedImageConstPtr msg) {
   cv_bridge::CvImagePtr cv_ptr;
@@ -32,20 +33,24 @@ void callback(const sensor_msgs::CompressedImageConstPtr msg) {
     
     double imageArea = cv_ptr->image.cols * cv_ptr->image.rows;
     double detectionProbability;
-    detection_msgs::BoundingBoxes bb_msg;
+    vision_msgs::Detection2DArray detections;
     // Draw rectangles around detected faces
     for (const auto &face : faces) {
       cv::rectangle(cv_ptr->image, face, cv::Scalar(0, 255, 0), 2);
       detectionProbability = (face.width * face.height) / imageArea;
-      detection_msgs::BoundingBox bb;
-      bb.probability = detectionProbability;
-      bb.xmin = face.x;
-      bb.ymin = face.y;
-      bb.xmax = face.x+face.width;
-      bb.ymax = face.y+face.height;
-      bb_msg.bounding_boxes.push_back(bb);
+      vision_msgs::Detection2D detection;
+      detection.bbox.center.x = (face.x + face.width)/2;
+      detection.bbox.center.y = (face.y + face.height)/2;
+      detection.bbox.size_x = face.width;
+      detection.bbox.size_y = face.height;
+
+      vision_msgs::ObjectHypothesisWithPose hypo;
+      hypo.id = 0;
+      hypo.score = detectionProbability;
+      detection.results.push_back(hypo);
+      detections.detections.push_back(detection);
     }
-    coord_pub.publish(bb_msg);
+    detect_pub.publish(detections);
   }
   cv_bridge::CvImage cv_image;
   cv_image.image = cv_ptr->image;
@@ -62,7 +67,7 @@ int main(int argc, char *argv[]) {
   faceCascade.load("/home/ubuntu/Desktop/ros_ws/src/alphabot/alphabot_ros/"
                    "alphabot_perception/face_detection/scripts/"
                    "haarcascade_frontalface_default.xml");
-  coord_pub = n.advertise<detection_msgs::BoundingBoxes>("/alphabot/face_detection/coord",1);
+  detect_pub = n.advertise<vision_msgs::Detection2DArray>("/alphabot/face_detection/bounding_boxes",1);
   img_pub = n.advertise<sensor_msgs::CompressedImage>("/alphabot/face_detection/image/compressed",1);
   ros::Subscriber sub = n.subscribe("/raspicam_node/image/compressed", 1, callback);
   ros::spin();
